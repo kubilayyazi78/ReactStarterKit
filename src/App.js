239fd31d2f9e653ee4a32dc9f20704c283429ff6
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-08-08 23:46:11 +0300

    Ortam değişkenleri test edildi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg"
 function App() {
   return (
     <div className="App">
+      <h3>{process.env.NODE_ENV}</h3>
+      <h3>{process.env.REACT_APP_API_URL}</h3>
       <img src="/logo192.png" alt=""></img>
       <img src={logo} alt=""></img>
       
