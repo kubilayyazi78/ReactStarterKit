0a8e00339eb0ed20390dc9f9d5a6a4e902284143
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-10-15 23:27:25 +0300

    Form elemanlarıyla çalışıldı.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,124 @@
-import { useReducer, useState, useCallback, useMemo } from "react";
-import todoReducer from "./reducers/todoReducer";
-import Header from "./Header";
-import AddToDo from "./AddToDo";
-import Todos from "./Todos";
+//import { useMemo } from "react";
+import { useState } from "react";
+
 function App() {
-  console.log("app rendered");
-  const [state, dispatch] = useReducer(todoReducer, {
-    todos: [],
-    todo: "",
-    search: ''
-  });
+  // const genders=['Erkek','Kadın']
+  const genders = [
+    { key: "1", value: "Erkek" },
+    { key: "2", value: "Kadın" },
+  ];
+  const categoryList = [
+    { key: 1, value: "Php" },
+    { key: 2, value: "JavaScript" },
+    { key: 3, value: "CSS" },
+    { key: 4, value: "HTML" },
+  ];
 
-  const [count, setCount] = useState(0);
+  const checkRule=(key,checked)=>{
+    setRules(rules=>rules.map(rule=>{
+      if(key===rule.key){
+        rule.checked=checked
+      }
+      return rule
+    }))
+  }
 
-  const submitHandle = useCallback(
-    (e) => {
-      e.preventDefault();
-      dispatch({
-        type: "ADD_TODO",
-        todo: state.todo,
-      });
-      //  setTodos([...todos, todo]);
-      //  setTodo('')
-    },
-    [state.todo]
-  );
-  const onChange = useCallback((e) => {
-    //setTodo(e.target.value)
-    dispatch({
-      type: "SET_TODO",
-      value: e.target.value,
-    });
-  }, []);
-  const searchHandle = (e) => {
-    dispatch({
-      type: "SET_SEARCH",
-      value: e.target.value,
-    });
-  };
 
-  const filteredTodos = useMemo(()=>{
-    return state.todos.filter(todo=>todo.toLocaleLowerCase('TR').includes(state.search.toLocaleLowerCase('TR')))
-  },[state.search, state.todos])
+  const [name, setName] = useState("kubilay");
+  const [description, setDescription] = useState("");
+  const [gender, setGender] = useState("1");
+  const [categories, setCategories] = useState([2, 4]);
+  const [rule, setRule] = useState(true);
+  const [rules, setRules] = useState([
+    { key: 1, value: "1. kural kabul", checked: false },
+    { key: 2, value: "2. kural kabul", checked: false },
+    { key: 3, value: "3. kural kabul", checked: true },
+  ]);
+  // const selectedGender=useMemo(()=>{
+  //   return genders[gender]
+  // },[gender])
 
+  const selectedGender = genders.find((g) => g.key === gender);
+  const selectedCategories =
+    categories && categoryList.filter((c) => categories.includes(c.key));
+  const enabled=rules.every(rule=>rule.checked)
   return (
     <>
-      <Header></Header>
-      <h3>{count}</h3>
-      <button onClick={() => setCount((count) => count + 1)}>Arttır</button>
-      <hr></hr>
-      <h1>To Do App</h1>
+      <button onClick={() => setName("Yazı")}>Değiştir</button>
       <input
         type="text"
-        value={state.search}
-        placeholder="Todolarda ara"
-        onChange={searchHandle}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       ></input>
-      {state.search}
-      <hr></hr>
-      <AddToDo
-        onChange={onChange}
-        submitHandle={submitHandle}
-        todo={state.todo}
-      ></AddToDo>
-      <Todos todos={filteredTodos}></Todos>
+      <br></br>
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+      <br></br>
+      <select value={gender} onChange={(e) => setGender(e.target.value)}>
+        {/* <option value="">Seçin</option>
+        <option value="1">Erkek</option>
+        <option value="2">Kadın</option> */}
+        {/* {genders.map((gender,index)=>(
+          <option value={index} key={index}>{gender}</option>
+        ))} */}
+        {genders.map((gender) => (
+          <option value={gender.key} key={gender.key}>
+            {gender.value}
+          </option>
+        ))}
+      </select>
+      <br></br>
+      {gender}
+      {/* {selectedGender} */}
+      <pre> {JSON.stringify(selectedGender, null, 2)}</pre>
+
+      <br></br>
+      <button onClick={() => setCategories([2, 3, 4])}>Kategoriler Seç</button>
+      <select
+        value={categories}
+        multiple={true}
+        onChange={(e) =>
+          setCategories(
+            [...e.target.selectedOptions].map((option) => +option.value)
+          )
+        }
+      >
+        {categoryList.map((category) => (
+          <option value={category.key} key={category.key}>
+            {category.value}
+          </option>
+        ))}
+      </select>
+      <pre> {JSON.stringify(categories, null, 2)}</pre>
+      <pre> {JSON.stringify(selectedCategories, null, 2)}</pre>
+
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          checked={rule}
+          onChange={(e) => setRule(e.target.checked)}
+        ></input>
+        Kuralları Kabul Ediyorum
+      </label>
+      <br></br>
+      <button disabled={!rule}>Devam Et</button>
+
+      <br></br>
+
+      {rules.map((rule) => (
+        <label key={rule.key}>
+          <input type="checkbox" checked={rule.checked} onChange={e =>checkRule(rule.key,e.target.checked)}></input>
+          {rule.value}
+        </label>
+      ))}
+      <br></br>
+      <pre> {JSON.stringify(rules, null, 2)}</pre>
+      <br></br>
+      <button disabled={!enabled}>Devam Et</button>
+
     </>
   );
 }
