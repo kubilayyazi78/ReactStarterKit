8b4df2eea5bd49e28cc666e72682e12c35bb7c6b
kubilayyazi78 <kubilayyazi@hotmail.com>
2023-02-05 18:00:07 +0300

    Helmet tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import { Helmet } from "react-helmet";
 function App() {
-  return (
-   <>
-   <div>
-    <Helmet>
-      <title>Ana Sayfa</title>
-      <meta name="description" content="anasayfa desc"></meta>
-    </Helmet>
-   </div>
-   </>
-  );
+  return;
 }
 
 export default App;
---
+++ src/Example11.js
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+function Example() {
+  return (
+    <>
+      <div>
+        <Helmet>
+          <title>Ana Sayfa</title>
+          <meta name="description" content="anasayfa desc"></meta>
+        </Helmet>
+      </div>
+    </>
+  );
+}
+export default Example;
